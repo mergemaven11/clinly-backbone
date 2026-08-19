3cc43907524e0c52c602a1e1b7363ce6ddb4ea58
Scott <38087336+mergemaven11@users.noreply.github.com>
2026-08-19 18:49:41 -0400

    Route marketing home and apply refreshed brand

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './AppV3.jsx'
+import MarketingPage from './MarketingPage.jsx'
 import './styles.css'
 import './platform-v3.css'
+import './brand-refresh.css'
+
+const isMarketingHome = window.location.pathname === '/' || window.location.pathname === ''
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {isMarketingHome ? <MarketingPage /> : <App />}
   </StrictMode>,
 )
