59906336a82cf710d34cdfd503d307e9742595d9
Scott <tobiascodes12@gmail.com>
2026-09-01 17:28:14 -0400

    Route unauthenticated visitors through marketing page

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,13 +1,39 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './AppV3.jsx'
+import { readSessionToken } from './brand'
+import LandingPage from './LandingPage.jsx'
 import './styles.css'
 import './platform-v3.css'
 import './business-layout-v2.css'
 
+function shouldOpenProduct(pathname = window.location.pathname) {
+  return Boolean(readSessionToken()) || pathname === '/login' || pathname === '/demo' || /^\/p\/[^/]+\/?$/.test(pathname)
+}
+
+function Root() {
+  const [openProduct, setOpenProduct] = useState(() => shouldOpenProduct())
+
+  useEffect(() => {
+    function handlePopState() {
+      setOpenProduct(shouldOpenProduct())
+    }
+    window.addEventListener('popstate', handlePopState)
+    return () => window.removeEventListener('popstate', handlePopState)
+  }, [])
+
+  function enterProduct(mode) {
+    const path = mode === 'demo' ? '/demo' : '/login'
+    window.history.pushState({}, '', path)
+    setOpenProduct(true)
+  }
+
+  return openProduct ? <App /> : <LandingPage onEnter={enterProduct} />
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
