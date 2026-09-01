868dc7149b025103bb5407c9987255bb55408434
Scott <tobiascodes12@gmail.com>
2026-09-01 17:21:57 -0400

    Fix provider business workspace layout
    * Add stable provider business layout overrides
    
    * Load provider business layout v2

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 import App from './AppV3.jsx'
 import './styles.css'
 import './platform-v3.css'
+import './business-layout-v2.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
