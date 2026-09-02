b4b009d7f1751956696f1527796137a7e4074be5
Tobias <tobiascodes12@gmail.com>
2026-09-02 11:36:02 -0400

    Expose public docs route

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './AppV3.jsx'
+import DocsPage from './DocsPage.jsx'
 import './styles.css'
 import './platform-v3.css'
 import './business-layout-v2.css'
 
+const isDocsRoute = /^\/docs\/?$/.test(window.location.pathname)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {isDocsRoute ? <DocsPage /> : <App />}
   </StrictMode>,
 )
